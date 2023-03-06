c41fe1e60330fc0214307516a8fe1a63306edeb6
MinaCode24 <anderson.mina.ix98@gmail.com>
2023-03-06 18:59:01 -0500

    Se agrego la segunda seccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
+import { Info1Component } from './components/info1/info1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    Info1Component
   ],
   imports: [
     BrowserModule,
